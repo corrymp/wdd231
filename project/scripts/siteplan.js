23fe750aa84b7f3cc53b7b84b49de7caab0bc63d
corrymp <corrympalmer@gmail.com>
2024-10-17 22:51:57 -0700

    W05 | Project Siteplan | initial comit

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,22 @@
+/*
+Color       Glow        Sign        Glow Edge
+White       #FFFFFF     #F9FFFE     #666666
+Light gray  #D3D3D3     #9D9D97     #545454
+Gray        #808080     #474F52     #333333
+Black       #000000     #1D1D21     #F0EBCC
+Red         #FF0000     #B02E26     #660000
+Brown       #8B4513     #835432     #371B07
+Orange      #FF681F     #F9801D     #66290C
+Yellow      #FFFF00     #FED83D     #666600
+Lime        #BFFF00     #80C71F     #4C6600
+Green       #00FF00     #5E7C16     #006600
+Cyan        #00FFFF     #169C9C     #006666
+Light blue  #9AC0CD     #3AB3DA     #3D4C52
+Blue        #0000FF     #3C44AA     #000066
+Purple      #A020F0     #8932B8     #400C60
+Magenta     #FF00FF     #C74EBD     #660066
+Pink        #FF69B4     #F38BAA     #662A48
+*/
+
+import { toggle } from "./utils.js";
+toggle('menu',['menu','nav'],'open')
